36ca5a5f425f495ec0dd238bc22f1384c1aea3af
bronco-one <zdzislaw.bronikowski@gmail.com>
2022-09-15 19:55:12 +0200

    no message

--- digital-clock/js/clock.js
+++ digital-clock/js/clock.js
@@ -6,8 +6,25 @@ const apT = document.getElementById('ap');
 
 
 function clock() {
+
     let now = new Date();
     let hour = now.getHours();
     let minute = now.getMinutes();
     let second = now.getSeconds();
+
+    hour = hour < 10 ? '0' + hour : hour;
+    minute = minute < 10 ? '0' + minute : minute;
+    second = second < 10 ? '0' + second : second;
+
+    hourT.innerHTML = hour;
+    minuteT.innerHTML = minute;
+    secondT.innerHTML = second;
+
+    apT.innerHTML = ap;
+
+    setTimeout(
+        () => { clock(); }, 1000
+    )
 }
+
+clock();
